5211e4ff11886508dd56e5bca55d03b615fbd21e
Dave Roberts <davjrob@amazon.de>
2024-07-04 13:44:54 +0200

    update

--- deployment/deploy-workshop.sh
+++ deployment/deploy-workshop.sh
@@ -4,6 +4,20 @@
 
 echo "Deploying workshop resources..."
 
+alias saasops_logs="aws logs tail --follow /aws/codebuild/install-workshop-stack-codebuild"
+
+saasops_deploy_wait() {
+    STATUS=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='SaaSOps'].StackStatus" --output text)
+    while [ $STATUS == 'CREATE_IN_PROGRESS' ]; do
+        echo "SaaSOps Stack status: "$STATUS $(date -Iseconds)
+        sleep 10
+        STATUS=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='SaaSOps'].StackStatus" --output text)
+    done
+    if [[ $STATUS == 'CREATE_COMPLETE' ]]; then
+        echo "Stack deploy complete"
+    fi
+}
+
 STACK_NAME="SaaSOps"${1:-""}
 REPO_URL="https://github.com/aws-samples/aws-saas-operations-workshop.git"
 REPO_BRANCH_NAME="davjrob"
@@ -21,3 +35,5 @@ STACK_ID=$(aws cloudformation create-stack \
 
 echo "STACK_ID: $STACK_ID"
 echo "CloudFormation stack $STACK_NAME creation started. You can monitor progress in the AWS Console at https://console.aws.amazon.com/cloudformation"
+
+saasops_deploy_wait
