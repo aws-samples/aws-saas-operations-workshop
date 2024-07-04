7dd08cf81f190fb0c341be2ecd3ab2b98bd2a40e
Dave Roberts <davjrob@amazon.de>
2024-07-04 13:32:16 +0200

    update

--- deployment/interface.sh
+++ deployment/interface.sh
@@ -11,7 +11,7 @@ saasops_test() {
     export LOAD_REGION=$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')
     export LOAD_USER_PASS=Lab@12345
 
-    export P1_METADATA=$(curl "${ADMIN_APIGATEWAYURL}/tenant/init/PooledTenant1")
+    export P1_METADATA=$(curl -s "${ADMIN_APIGATEWAYURL}/tenant/init/PooledTenant1")
     export P1_USERPOOL=$(echo $P1_METADATA | jq -r '.userPoolId')
     export P1_CLIENTID=$(echo $P1_METADATA | jq -r '.appClientId')
     export P1_ENDPOINT=$(echo $P1_METADATA | jq -r '.apiGatewayUrl')
@@ -22,6 +22,27 @@ saasops_test() {
         --client-id $P1_CLIENTID --auth-parameters \
         USERNAME=$P1_USER,PASSWORD=$LOAD_USER_PASS | jq -r .AuthenticationResult.IdToken)
 
-    curl -X GET -H "Authorization: Bearer ${P1_TOKEN}" "${P1_ENDPOINT}/products"
-    curl -X GET -H "Authorization: Bearer ${P1_TOKEN}" "${P1_ENDPOINT}/orders"
+    PRODUCTS=$(curl -s -X GET -H "Authorization: Bearer ${P1_TOKEN}" "${P1_ENDPOINT}/products")
+    ORDERS=$(curl -s -X GET -H "Authorization: Bearer ${P1_TOKEN}" "${P1_ENDPOINT}/orders")
+    if [[ $PRODUCTS > 1 ]]
+        then
+            echo "Products found"
+            if [[ $ORDERS > 1 ]]; then echo "Orders found"
+                else echo "Orders not found. Have you initialised the workshop? Check deployment."
+            fi
+        else
+            echo "Products not found. Have you initialised the workshop? Check deployment."
+    fi
+}
+
+saasops_deploy_wait() {
+    STATUS=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='SaaSOps'].StackStatus" --output text)
+    while [ $STATUS == 'CREATE_IN_PROGRESS' ]; do
+        echo "SaaSOps Stack status: "$STATUS $(date -Iseconds)
+        sleep 10
+        STATUS=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='SaaSOps'].StackStatus" --output text)
+    done
+    if [[ $STATUS == 'CREATE_COMPLETE' ]]; then
+        echo "Stack deploy complete"
+    fi
 }
